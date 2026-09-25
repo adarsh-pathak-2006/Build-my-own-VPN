72be9dd6a408a176339d6d663dc84115c5fc6ed3
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-25 15:28:33 +0530

    added urls in the app

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import OtpCreationAPI
+from .views import OtpCreationAPI, OtpVerificationAPI, PasswordSetupAPI
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register/', OtpCreationAPI.as_view()),
+    path('Otp-creation/', OtpCreationAPI.as_view()),
+    path('otp-verify/', OtpVerificationAPI.as_view()),
+    path('password-setup/', PasswordSetupAPI.as_view()),
     path('api/token/', TokenObtainPairView.as_view()),
     path('apt/token/refresh/', TokenRefreshView.as_view()),
 ]
