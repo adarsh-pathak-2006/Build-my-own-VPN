fa0a122108d3d87dc7e33535cc4c1448add779c9
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-26 19:36:59 +0530

    made changes in the task so that it asks otp service for otp

--- authentication/tasks.py
+++ authentication/tasks.py
@@ -1,12 +1,11 @@
 from celery import shared_task
 from django.core.cache import cache
-import random
-import time
 from config.cache_keys import otpCacheKey
+import requests
+from django.conf import settings
 
 @shared_task
 def OTPCreation(id, email):
-    time.sleep(5)
-    otp=str(random.randint(11111111, 99999999))
-    cache.set(otpCacheKey(id=id), otp, timeout=500)
-    return f"otp sent to {email} otp:{otp}"
+    response=requests.post(settings.OTP_SERVICE_URL, json={"email":email})
+    cache.set(otpCacheKey(id=id), response.json().get("otp"), timeout=500)
+    return f"otp sent to {email} otp_data here: {response.json()}"
