6980f550513c3e87cc40a82f47c3ca318554011b
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-26 19:27:03 +0530

    added pagination to reduce db load

--- core/serializer.py
+++ core/serializer.py
@@ -2,9 +2,15 @@ from rest_framework.serializers import ModelSerializer
 from .models import Summary
 from authentication.serializer import UserGetSerializer
 
+class SummaryGenerationListSerializer(ModelSerializer):
+    class Meta:
+        model=Summary
+        fields=['user', 'link', 'time']
+        read_only_fields=['user', 'link', 'time']
+
 class SummaryGenerationSerializer(ModelSerializer):
     user=UserGetSerializer(read_only=True)
     class Meta:
         model=Summary
-        fields=['link']
+        fields='__all__'
         read_only_fields=['transcript', 'summary', 'time']
--- core/views.py
+++ core/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
 from .models import Summary
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .tasks import TranscriptFetch
-from .serializer import SummaryGenerationSerializer
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .serializer import SummaryGenerationSerializer, SummaryGenerationListSerializer
+from rest_framework.generics import RetrieveAPIView
+from config.pagination import GeneralPagination
 
-class GeneratedSummaryListAPI(ListAPIView):
-    serializer_class=SummaryGenerationSerializer
-
-    def get_queryset(self):
-        return Summary.objects.select_related('user').filter(user=self.request.user)
+class GeneratedSummaryListAPI(APIView):
+    def get(self, request):
+        paginator=GeneralPagination()
+        data=paginator.paginate_queryset(Summary.objects.select_related('user').filter(user=self.request.user).order_by("-time"))
+        serial=SummaryGenerationListSerializer(data, many=True)
+        return paginator.get_paginated_response(serial.data)
+        return response
 
 class GeneratedSummaryDetailAPI(RetrieveAPIView):
     serializer_class=SummaryGenerationSerializer
